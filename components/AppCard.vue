15aafc8198e07a744dca52a5d51cb3076bea1025
Myka Peligrino <mykapeligrino@gmail.com>
2022-12-06 04:01:20 -0700

    cleanup

--- components/AppCard.vue
+++ components/AppCard.vue
@@ -2,7 +2,18 @@
   <div class="flex flex-col justify-center align-center text-center">
     <img src="@/assets/images/jewelry.jpg" class="mx-auto p-5" alt="three golden rings" height="80" width="600">
 
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas rem non assumenda modi laudantium eos dolore sed debitis voluptatem fugit?</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas rem non assumenda modi laudantium eos dolore sed debitis voluptatem fugit?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci recusandae architecto esse distinctio nostrum ut dignissimos quos saepe minima, ad deleniti dolorem magni sequi qui modi quis iure maxime fugit fugiat cum delectus est iusto! Explicabo, accusamus maxime voluptate eaque, ratione culpa illo ullam voluptas voluptatem distinctio quas pariatur, fugit soluta. Excepturi temporibus magnam veritatis corporis asperiores blanditiis soluta doloribus maiores, ratione est odit voluptatum obcaecati aspernatur tenetur tempora eveniet! Nemo, velit? Tenetur repellendus, laudantium, ipsum sed veniam velit earum maiores aut dolorem quos, inventore minus illo? Repudiandae veniam animi qui dolores, eaque expedita deserunt.</p>
   </div>
 </template>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    display: block;
+    margin: auto;
+    max-width: 65ch;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <AppSection />
+    <!-- <AppSection /> -->
 
-    <!-- <AppCard /> -->
+    <AppCard />
   </div>
 </template>
 
