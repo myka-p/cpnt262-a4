6c3a75cfa1dc55175d3a2806f83e44fdc514063b
Myka Peligrino <mykapeligrino@gmail.com>
2022-12-06 04:13:03 -0700

    changed images

--- components/AppSection.vue
+++ components/AppSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center align-center text-center">
-    <img src="@/assets/images/wall.jpg" class="mx-auto p-5" alt="minimalistic shelf" height="80" width="600">
+    <img src="@/assets/images/vase.jpg" class="mx-auto p-5" alt="hand touching a vase" height="80" width="600">
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci recusandae architecto esse distinctio nostrum ut dignissimos quos saepe minima, ad deleniti dolorem magni sequi qui modi quis iure maxime fugit fugiat cum delectus est iusto! Explicabo, accusamus maxime voluptate eaque, ratione culpa illo ullam voluptas voluptatem distinctio quas pariatur, fugit soluta. Excepturi temporibus magnam veritatis corporis asperiores blanditiis soluta doloribus maiores, ratione est odit voluptatum obcaecati aspernatur tenetur tempora eveniet! Nemo, velit? Tenetur repellendus, laudantium, ipsum sed veniam velit earum maiores aut dolorem quos, inventore minus illo? Repudiandae veniam animi qui dolores, eaque expedita deserunt.</p>
   </div>
--- pages/about.vue
+++ pages/about.vue
@@ -1,23 +1,6 @@
-<!-- This is the home page -->
-<script setup>
-
-</script>
-
 <template>
   <div>
-    <h2>About</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas rem non assumenda modi laudantium eos dolore sed debitis voluptatem fugit?</p>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas rem non assumenda modi laudantium eos dolore sed debitis voluptatem fugit?</p>
+    <AppSection />
   </div>
 
 </template>
-
-<style scoped>
-  h2 {
-    margin-bottom: 20px;
-    font-size: 36px;
-  }
-  p {
-    margin: 20px 0;
-  }
-</style>
