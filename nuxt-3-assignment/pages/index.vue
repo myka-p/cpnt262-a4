8b85eef493a801a1d0b3361119095997dafd25dc
Myka Peligrino <mykapeligrino@gmail.com>
2022-12-06 01:23:37 -0700

    trial for netlify deploy

---
+++ nuxt-3-assignment/pages/index.vue
@@ -0,0 +1,23 @@
+<!-- This is the home page -->
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas rem non assumenda modi laudantium eos dolore sed debitis voluptatem fugit?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas rem non assumenda modi laudantium eos dolore sed debitis voluptatem fugit?</p>
+  </div>
+
+</template>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
---
+++ nuxt-3-assignment/pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aspernatur, a provident, rerum consequatur in quod vero voluptate unde ratione error temporibus, tempora maxime asperiores. Consequuntur facilis aliquam quia consequatur.</p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+
+  definePageMeta({
+    layout: 'products'
+  })
+</script>
+
+<style scoped>
+
+</style>
