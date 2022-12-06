0472a557c2d37f0d5fab50c1b262ca55111d1f11
Myka Peligrino <mykapeligrino@gmail.com>
2022-12-06 04:17:32 -0700

    header change

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -2,7 +2,7 @@
   <div>
     <header class="bg-gradient-to-l from-teal-700 via-neutral-400 to-stone-600">
       <nav class="container mx-auto p-4 flex justify-between">
-        <nuxt-link to="/" class="font-bold lg:text-5xl">Andromeda</nuxt-link>
+        <nuxt-link to="/" class="font-bold lg:text-5xl">The Haven</nuxt-link>
         <ul class="flex gap-10 text-2xl">
           <li><nuxt-link to="/">Home</nuxt-link></li>
           <li><nuxt-link to="/about">About</nuxt-link></li>
